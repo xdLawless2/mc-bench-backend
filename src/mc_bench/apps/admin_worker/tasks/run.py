1be9ed600fcb33488f7e17c95141040e49abf7d2
hunter <huntcsg@gmail.com>
2024-11-24 20:29:09 -0500

    Fix database connection leak in celery worker. Autoformat. Remove unused route in api and admin-api.

--- src/mc_bench/apps/admin_worker/tasks/run.py
+++ src/mc_bench/apps/admin_worker/tasks/run.py
@@ -4,7 +4,7 @@ import time
 from sqlalchemy import select
 
 from mc_bench.models.run import RUN_STATE, Run, Sample, run_state_id_for
-from mc_bench.util.postgres import get_session
+from mc_bench.util.postgres import get_managed_session
 
 from ..app import app
 
@@ -13,29 +13,29 @@ from ..app import app
 def execute_prompt(
     run_id,
 ):
-    db = get_session()
-    run = db.scalar(select(Run).where(Run.id == run_id))
+    with get_managed_session() as db:
+        run = db.scalar(select(Run).where(Run.id == run_id))
 
-    response = run.model.default_provider.execute_prompt(
-        template=run.template, prompt=run.prompt
-    )
+        response = run.model.default_provider.execute_prompt(
+            template=run.template, prompt=run.prompt
+        )
 
-    sample_kwargs = {
-        "created_by": run.created_by,
-        "run_id": run.id,
-        "raw": response,
-    }
+        sample_kwargs = {
+            "created_by": run.created_by,
+            "run_id": run.id,
+            "raw": response,
+        }
 
-    sample = Sample(**sample_kwargs)
-    db.add(sample)
-    db.commit()
+        sample = Sample(**sample_kwargs)
+        db.add(sample)
+        db.commit()
 
-    run.state_id = run_state_id_for(db, RUN_STATE.PROMPT_COMPLETED)
-    db.commit()
+        run.state_id = run_state_id_for(db, RUN_STATE.PROMPT_COMPLETED)
+        db.commit()
 
-    db.refresh(sample)
+        db.refresh(sample)
 
-    return sample.id
+        return sample.id
 
 
 @app.task(name="run.parse_prompt")
@@ -45,54 +45,54 @@ def parse_prompt(
     if sample_id is None:
         raise RuntimeError("sample_id is required")
 
-    db = get_session()
-    sample = db.scalar(select(Sample).where(Sample.id == sample_id))
-    run = db.scalar(select(Run).where(Run.id == sample.run_id))
+    with get_managed_session() as db:
+        sample = db.scalar(select(Sample).where(Sample.id == sample_id))
+        run = db.scalar(select(Run).where(Run.id == sample.run_id))
 
-    print("sample: ", sample)
-    print("sample.raw:", sample.raw)
+        print("sample: ", sample)
+        print("sample.raw:", sample.raw)
 
-    # 2. Parse Response
-    parsed = _parse_known_parts(sample.raw)
+        # 2. Parse Response
+        parsed = _parse_known_parts(sample.raw)
 
-    if parsed.get("code"):
-        print("Code in parsed")
+        if parsed.get("code"):
+            print("Code in parsed")
 
-        if "```" in parsed["code"]:
-            start_index = parsed["code"].find("```")
-            end_index = parsed["code"].index("```", start_index + 1)
-            code = parsed["code"][start_index:end_index]
-            code_lines = []
-            for line in code.split("\n"):
-                if "```" in line:
-                    continue
-                code_lines.append(line)
-            print("num code lines: ", len(code_lines))
-            parsed["code"] = "\n".join(code_lines)
+            if "```" in parsed["code"]:
+                start_index = parsed["code"].find("```")
+                end_index = parsed["code"].index("```", start_index + 1)
+                code = parsed["code"][start_index:end_index]
+                code_lines = []
+                for line in code.split("\n"):
+                    if "```" in line:
+                        continue
+                    code_lines.append(line)
+                print("num code lines: ", len(code_lines))
+                parsed["code"] = "\n".join(code_lines)
 
-        sample.result_code_text = parsed["code"].strip()
+            sample.result_code_text = parsed["code"].strip()
 
-    if parsed.get("inspiration"):
-        sample.result_inspiration_text = parsed["inspiration"].strip()
+        if parsed.get("inspiration"):
+            sample.result_inspiration_text = parsed["inspiration"].strip()
 
-    if parsed.get("description"):
-        sample.result_description_text = parsed["description"].strip()
+        if parsed.get("description"):
+            sample.result_description_text = parsed["description"].strip()
 
-    if not all(
-        [parsed.get("code"), parsed.get("inspiration"), parsed.get("description")]
-    ):
-        new_state = run_state_id_for(db, RUN_STATE.PROMPT_PROCESSING_FAILED)
-    else:
-        new_state = run_state_id_for(db, RUN_STATE.PROMPT_COMPLETED)
+        if not all(
+            [parsed.get("code"), parsed.get("inspiration"), parsed.get("description")]
+        ):
+            new_state = run_state_id_for(db, RUN_STATE.PROMPT_PROCESSING_FAILED)
+        else:
+            new_state = run_state_id_for(db, RUN_STATE.PROMPT_COMPLETED)
 
-    db.commit()
-    run.state_id = new_state
-    db.commit()
+        db.commit()
+        run.state_id = new_state
+        db.commit()
 
-    if new_state == run_state_id_for(db, RUN_STATE.PROMPT_PROCESSING_FAILED):
-        raise RuntimeError("Prompting didn't go well")
+        if new_state == run_state_id_for(db, RUN_STATE.PROMPT_PROCESSING_FAILED):
+            raise RuntimeError("Prompting didn't go well")
 
-    return sample_id
+        return sample_id
 
 
 def _parse_known_parts(text):
@@ -139,25 +139,25 @@ def build_structure(sample_id):
 
     time.sleep(15)
 
-    db = get_session()
-    sample = db.scalar(select(Sample).where(Sample.id == sample_id))
-    run = db.scalar(select(Run).where(Run.id == sample.run_id))
+    with get_managed_session() as db:
+        sample = db.scalar(select(Sample).where(Sample.id == sample_id))
+        run = db.scalar(select(Run).where(Run.id == sample.run_id))
 
-    run.state_id = run_state_id_for(db, RUN_STATE.BUILD_COMPLETED)
-    db.commit()
+        run.state_id = run_state_id_for(db, RUN_STATE.BUILD_COMPLETED)
+        db.commit()
 
-    # app.send_task(
-    #     "run.post_processing",
-    #     kwargs=dict(
-    #         run_id=run.id,
-    #     ),
-    #     queue="admin",
-    # )
+        # app.send_task(
+        #     "run.post_processing",
+        #     kwargs=dict(
+        #         run_id=run.id,
+        #     ),
+        #     queue="admin",
+        # )
 
-    run.state_id = run_state_id_for(db, RUN_STATE.POST_PROCESSING_ENQUEUED)
-    db.commit()
+        run.state_id = run_state_id_for(db, RUN_STATE.POST_PROCESSING_ENQUEUED)
+        db.commit()
 
-    return sample_id
+        return sample_id
 
 
 @app.task(name="run.post_processing")
@@ -166,25 +166,26 @@ def post_process_build(sample_id):
         raise RuntimeError("sample_id is required")
 
     time.sleep(15)
-    db = get_session()
-    sample = db.scalar(select(Sample).where(Sample.id == sample_id))
-    run = db.scalar(select(Run).where(Run.id == sample.run_id))
 
-    run.state_id = run_state_id_for(db, RUN_STATE.POST_PROCESSING_COMPLETED)
-    db.commit()
+    with get_managed_session() as db:
+        sample = db.scalar(select(Sample).where(Sample.id == sample_id))
+        run = db.scalar(select(Run).where(Run.id == sample.run_id))
 
-    # app.send_task(
-    #     "run.sample_prep",
-    #     kwargs=dict(
-    #         run_id=run.id,
-    #     ),
-    #     queue="admin",
-    # )
+        run.state_id = run_state_id_for(db, RUN_STATE.POST_PROCESSING_COMPLETED)
+        db.commit()
 
-    run.state_id = run_state_id_for(db, RUN_STATE.SAMPLE_PREP_ENQUEUED)
-    db.commit()
+        # app.send_task(
+        #     "run.sample_prep",
+        #     kwargs=dict(
+        #         run_id=run.id,
+        #     ),
+        #     queue="admin",
+        # )
 
-    return sample_id
+        run.state_id = run_state_id_for(db, RUN_STATE.SAMPLE_PREP_ENQUEUED)
+        db.commit()
+
+        return sample_id
 
 
 @app.task(name="run.sample_prep")
@@ -193,11 +194,12 @@ def prepare_sample(sample_id):
         raise RuntimeError("sample_id is required")
 
     time.sleep(15)
-    db = get_session()
-    sample = db.scalar(select(Sample).where(Sample.id == sample_id))
-    run = db.scalar(select(Run).where(Run.id == sample.run_id))
 
-    run.state_id = run_state_id_for(db, RUN_STATE.COMPLETED)
-    db.commit()
+    with get_managed_session() as db:
+        sample = db.scalar(select(Sample).where(Sample.id == sample_id))
+        run = db.scalar(select(Run).where(Run.id == sample.run_id))
 
-    return sample_id
+        run.state_id = run_state_id_for(db, RUN_STATE.COMPLETED)
+        db.commit()
+
+        return sample_id
--- src/mc_bench/apps/api/app.py
+++ src/mc_bench/apps/api/app.py
@@ -2,12 +2,8 @@ import os
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy import text
 
-from mc_bench.apps.api.celery import send_task
 from mc_bench.apps.api.routers.user import user_router
-from mc_bench.util.postgres import get_session
-from mc_bench.util.redis import RedisDatabase, get_redis_client
 
 allow_origins = os.environ.get("CORS_ALLOWED_ORIGIN", "").split(",")
 
@@ -22,24 +18,3 @@ app.add_middleware(
 )
 
 app.include_router(user_router)
-
-
-@app.get("/")
-def read_root():
-    db = get_session()
-    should_by_one = db.execute(text("select 1")).scalar()
-    print(should_by_one)
-    db.close()
-
-    redis = get_redis_client(RedisDatabase.CACHE)
-    redis_connected = bool(redis.ping())
-
-    task = send_task("example_task")
-    task_result = task.get()
-
-    return {
-        "Hello": "World",
-        "value": should_by_one,
-        "redis_connected": redis_connected,
-        "task_result": task_result,
-    }
