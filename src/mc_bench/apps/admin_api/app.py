e26b66c4fd5d3a9e542a5de5cfe1f44541000264
Hunter <huntcsg@users.noreply.github.com>
2024-11-24 20:51:12 -0500

    Revert "Fix database connection leak in celery worker. Autoformat. Remove unu…"
    This reverts commit 1be9ed600fcb33488f7e17c95141040e49abf7d2.

--- src/mc_bench/apps/admin_api/app.py
+++ src/mc_bench/apps/admin_api/app.py
@@ -2,12 +2,16 @@ import os
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import text
 
+from mc_bench.apps.admin_api.celery import send_task
 from mc_bench.apps.admin_api.routers.generations import generation_router
 from mc_bench.apps.admin_api.routers.models import model_router
 from mc_bench.apps.admin_api.routers.prompts import prompt_router
 from mc_bench.apps.admin_api.routers.runs import run_router
 from mc_bench.apps.admin_api.routers.templates import template_router
+from mc_bench.util.postgres import get_session
+from mc_bench.util.redis import RedisDatabase, get_redis_client
 
 app = FastAPI()
 app.add_middleware(
@@ -22,3 +26,24 @@ app.include_router(prompt_router)
 app.include_router(model_router)
 app.include_router(generation_router)
 app.include_router(run_router)
+
+
+@app.get("/")
+def read_root():
+    db = get_session()
+    should_by_one = db.execute(text("select 1")).scalar()
+    print(should_by_one)
+    db.close()
+
+    redis = get_redis_client(RedisDatabase.CACHE)
+    redis_connected = bool(redis.ping())
+
+    task = send_task("admin_example_task")
+    task_result = task.get()
+
+    return {
+        "Hello": "World",
+        "value": should_by_one,
+        "redis_connected": redis_connected,
+        "task_result": task_result,
+    }
--- src/mc_bench/migrations/versions/6d48eca377ff_add_proper_roles_for_writing_to_sample_.py
+++ src/mc_bench/migrations/versions/6d48eca377ff_add_proper_roles_for_writing_to_sample_.py
@@ -5,14 +5,15 @@ Revises: e1181f674ab9
 Create Date: 2024-11-24 17:13:33.047217
 
 """
-
 from typing import Sequence, Union
 
 from alembic import op
+import sqlalchemy as sa
+
 
 # revision identifiers, used by Alembic.
-revision: str = "6d48eca377ff"
-down_revision: Union[str, None] = "e1181f674ab9"
+revision: str = '6d48eca377ff'
+down_revision: Union[str, None] = 'e1181f674ab9'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
