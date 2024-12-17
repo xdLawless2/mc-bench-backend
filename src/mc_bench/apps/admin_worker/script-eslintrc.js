3611265bc1c88d88e059ab0297e42a8a77165429
hunter <huntcsg@gmail.com>
2024-12-17 11:58:18 -0500

    Remove warnings and relax linting requirements

--- src/mc_bench/apps/admin_worker/script-eslintrc.js
+++ src/mc_bench/apps/admin_worker/script-eslintrc.js
@@ -9,17 +9,18 @@ module.exports = {
     },
     rules: {
         // Catch syntax errors and undefined variables
-        "no-undef": "error",
-        "no-unused-vars": "warn",
-        
-        // Basic syntax preferences
-        "semi": ["error", "always"],
-        "no-extra-semi": "warn",
-        
-        // Common error catches
-        "no-unreachable": "error",
-        "no-constant-condition": "error",
-        "no-dupe-args": "error",
-        "no-dupe-keys": "error"
+        "no-undef": "error",                    // Disallow undeclared variables
+        "no-unreachable": "error",              // Disallow unreachable code
+        "no-constant-condition": "error",       // Disallow constant conditions in loops/if
+        "no-dupe-args": "error",                // Disallow duplicate function parameters
+        "no-dupe-keys": "error",                // Disallow duplicate object keys
+        "no-dupe-else-if": "error",             // Disallow duplicate conditions in if-else chains
+        "no-func-assign": "error",              // Disallow reassigning function declarations
+        "no-import-assign": "error",            // Disallow assigning to imported bindings
+        "no-obj-calls": "error",                // Disallow calling global objects as functions
+        "no-sparse-arrays": "error",            // Disallow sparse arrays with empty slots
+        "no-unexpected-multiline": "error",     // Disallow confusing multiline expressions
+        "use-isnan": "error",                   // Require using isNaN() to check for NaN
+        "valid-typeof": "error"                 // Enforce comparing typeof with valid strings
     }
 };
