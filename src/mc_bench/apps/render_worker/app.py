380ee315554bc99df42c58215ae473cf2fea77c3
hunter <huntcsg@gmail.com>
2025-02-04 13:32:30 -0500

    reduce render worker prefetch to 1

--- src/mc_bench/apps/render_worker/app.py
+++ src/mc_bench/apps/render_worker/app.py
@@ -9,7 +9,7 @@ from mc_bench.util.celery import make_worker_celery_app
 
 app = make_worker_celery_app(
     dict(
-        worker_prefetch_multiplier=4,
+        worker_prefetch_multiplier=1,
     )
 )
 
