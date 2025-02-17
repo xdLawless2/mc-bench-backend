f37976e832cfdb63cf5debf03ad449879d9bd5a6
hunter <huntcsg@gmail.com>
2025-02-17 01:13:06 -0500

    Added build delay configuration - set default to 25 ms

--- src/mc_bench/apps/server_worker/config.py
+++ src/mc_bench/apps/server_worker/config.py
@@ -6,6 +6,6 @@ class Settings:
     EXPORT_STRUCTURE_VIEWS = os.environ.get("EXPORT_STRUCTURE_VIEWS", "false") == "true"
     EXPOSE_SERVER_PORTS = os.environ.get("EXPOSE_SERVER_PORTS", "false") == "true"
     HUMANIZE_LOGS = os.environ.get("HUMANIZE_LOGS", "false") == "true"
-    BUILD_DELAY = os.environ.get("BUILD_DELAY_MS", "75")
+    BUILD_DELAY = os.environ.get("BUILD_DELAY_MS", "25")
 
 settings = Settings()
