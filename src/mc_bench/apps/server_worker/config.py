abb37930c6c5a2a7b93849ee3cf131123cfffa5f
hunter <huntcsg@gmail.com>
2025-02-19 12:59:06 -0500

    Upgraded prismarine-viewer in the builder and re-enabled content export task (set to default)

--- src/mc_bench/apps/server_worker/config.py
+++ src/mc_bench/apps/server_worker/config.py
@@ -3,7 +3,7 @@ import os
 
 class Settings:
     INTERNAL_OBJECT_BUCKET = os.environ["INTERNAL_OBJECT_BUCKET"]
-    EXPORT_STRUCTURE_VIEWS = os.environ.get("EXPORT_STRUCTURE_VIEWS", "false") == "true"
+    EXPORT_STRUCTURE_VIEWS = os.environ.get("EXPORT_STRUCTURE_VIEWS", "true") == "true"
     EXPOSE_SERVER_PORTS = os.environ.get("EXPOSE_SERVER_PORTS", "false") == "true"
     HUMANIZE_LOGS = os.environ.get("HUMANIZE_LOGS", "false") == "true"
     BUILD_DELAY = os.environ.get("BUILD_DELAY_MS", "25")
